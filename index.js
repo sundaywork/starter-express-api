0945db9d9d1464d1869961f85e30240d8b777440
Gewei Zhang <gewei.zhang@utecture.com>
2023-01-03 18:27:49 +1300

    save ownerId

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/api/save-subscription/', async function (req, res) {
     )
 
     // Check the request body has at least an endpoint.
-    if (!req.body || !req.body.endpoint) {
+    if (!req.body || !req.body.subscription || !req.body.subscription.endpoint) {
         // Not a valid subscription.
         res.status(400);
         res.setHeader('Content-Type', 'application/json');
@@ -59,39 +59,39 @@ app.post('/api/save-subscription/', async function (req, res) {
         return false;
     }
 
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
 
-    console.log(req.body);
-    res.send(req.body);
 
-    // return await saveSubscriptionToDatabase(req.body)
-    // .then(function (subscriptionId) {
-    //   res.setHeader('Content-Type', 'application/json');
-    //   res.send(JSON.stringify({data: {success: true}}));
-    // })
-    // .catch(function (err) {
-    //   res.status(500);
-    //   res.setHeader('Content-Type', 'application/json');
-    //   res.send(
-    //     JSON.stringify({
-    //       error: {
-    //         id: 'unable-to-save-subscription',
-    //         message:
-    //           'The subscription was received but we were unable to save it to our database.',
-    //       },
-    //     }),
-    //   );
-    // });
+    
+    //res.send(req.body);
+
+    // {"ownerId":"ss","subscription":{"endpoint":"https://fcm.googleapis.com/fcm/send/dqy_o-vmruA:APA91bEbzBiquM_vAQXEOfFgfq4O3DQvQB0XScNTnDZPwFxjNbx23T1ehsl-Bntcroa7acDAThEkAQFWUcZpsVB3CX5prFm10G8n3qJTjM8fbUZ79hvSlRmfI4cDJ1w_by5iPR4XUPjh","expirationTime":null,"keys":{"p256dh":"BHt55lq5fttaOHup_aSSnj4BLXtK4A6QwEUMJXMqV5608GFpRA7ut5k469fb72QRFG8hlFtLQ2p9ldoDv9UoLM0","auth":"oNaY-obF4Wpku4pdDcCUOQ"}}}
+
+    return await saveSubscriptionToDatabase(req.body)
+    .then(function (subscriptionId) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({data: {success: true, ownerId: subscriptionId}}));
+    })
+    .catch(function (err) {
+      res.status(500);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(
+        JSON.stringify({
+          error: {
+            id: 'unable-to-save-subscription',
+            message:
+              'The subscription was received but we were unable to save it to our database.',
+          },
+        }),
+      );
+    });
 });
 
-async function saveSubscriptionToDatabase(subscription) {
+async function saveSubscriptionToDatabase(payload) {
 
-    let user = await subscribers.set('leo', subscription);
-    return user._id;
+    let ownerId = payload.ownerId;
+    let subscription = payload.subscription;
+    let user = await subscribers.set(ownerId, subscription);
+    return ownerId;
 }
 
 app.listen(process.env.PORT || 3000)
